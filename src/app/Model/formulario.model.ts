8ac3a315ab63bdd9e56b43d23eadfdbc46b28791
Victor <elaburrimientomanda@gmail.com>
2020-09-07 22:25:06 +0200

    Tabla con datos

---
+++ src/app/Model/formulario.model.ts
@@ -0,0 +1,15 @@
+export class formulario {
+  categoria: string;
+  colectivo: string;
+  direccion: string;
+  dni: string;
+  email: string;
+  nombre: string;
+  p_covid1: string;
+  p_covid2: string;
+  p_covid3: string;
+  p_covid4: string;
+  p_covid5: string;
+  p_covid6: string;
+  telefono: any;
+}
--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from 'src/app/informacion.service';
+import { formulario } from 'src/app/Model/formulario.model';
 
 @Component({
   selector: 'app-informacion',
@@ -6,40 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informacion.component.css']
 })
 export class InformacionComponent implements OnInit {
-Usuario=[
-{
-  nombre:'Victor',
-  dni:'30235098T',
-  email:'elaburrimientomanda@gmail.com',
-  direccion:'c/Siracusa bloque 7',
-  categoria:'alevin',
-},
-{
-  nombre:'Victor',
-  dni:'30235098T',
-  email:'elaburrimientomanda@gmail.com',
-  direccion:'c/Siracusa bloque 7',
-  categoria:'alevin',
-},
-{
-  nombre:'Victor',
-  dni:'30235098T',
-  email:'elaburrimientomanda@gmail.com',
-  direccion:'c/Siracusa bloque 7',
-  categoria:'alevin',
-},
-{
-  nombre:'Victor',
-  dni:'30235098T',
-  email:'elaburrimientomanda@gmail.com',
-  direccion:'c/Siracusa bloque 7',
-  categoria:'alevin',
-}
+  _formulario: formulario[];
 
-]
-  constructor() { }
+  constructor(private _informacionService: InformacionService) {
+
+   }
 
   ngOnInit(): void {
-  }
+    this._informacionService.getDatosFormulario().subscribe(data => {
+      this._formulario = data.map(e => {
+        return {
+          categoria: e.payload.doc.Categoria,
+          colectivo: e.payload.doc.data().Colectivo,
+          direccion: e.payload.doc.data().Direccion,
+          dni: e.payload.doc.data().Dni,
+          email: e.payload.doc.data().Email,
+          nombre: e.payload.doc.data().Nombre,
+          p_covid1: e.payload.doc.data().p_covid1,
+          p_covid2: e.payload.doc.data().p_covid2,
+          p_covid3: e.payload.doc.data().p_covid3,
+          p_covid4: e.payload.doc.data().p_covid4,
+          p_covid5: e.payload.doc.data().p_covid5,
+          p_covid6: e.payload.doc.data().p_covid6,
+          telefono: e.payload.doc.data().Telefono
+        } as unknown as formulario;
+    });
+console.log(this._formulario);
+  });}
 
-}
+  }
