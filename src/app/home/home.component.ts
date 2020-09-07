d35bc65665a9594e272275eb7d11b317ad229e10
Carlos <se_carlosprieto@hotmail.com>
2020-09-07 23:55:32 +0200

    AAAAAAAAAAAA

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,9 +30,9 @@ export class HomeComponent implements OnInit {
 
   constructor(private authSvc: AuthService, private router: Router) { }
 
-   titulo1:string;
+   
   ngOnInit(): void {
-    this.titulo1="¿Ha viajado con un paciente COVID-19 en cualquier medio de transporte?";
+    
   }
   onSubmit() {
     let Record = {};
@@ -42,7 +42,7 @@ export class HomeComponent implements OnInit {
     Record['Direccion'] = this.homeForm.value.direccion;
     Record['Telefono'] = this.homeForm.value.telefono;
     Record['Email'] = this.homeForm.value.email;
-    Record['Paises/CCAA visitados'] = this.homeForm.value.paises;
+    Record['Paises_CCAA_visitados'] = this.homeForm.value.paises;
     Record['Categoria'] = this.homeForm.value.select_categoria;
     Record['p_covid1'] = this.homeForm.get('radioPregunta1').value;
     Record['p_covid2'] = this.homeForm.get('radioPregunta2').value;
--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InformacionService } from 'src/app/informacion.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { formulario } from 'src/app/Model/formulario.model';
 
 @Component({
@@ -10,15 +11,19 @@ import { formulario } from 'src/app/Model/formulario.model';
 export class InformacionComponent implements OnInit {
   _formulario: formulario[];
 
+  informationForm = new FormGroup({
+    nombreFilter: new FormControl('')
+  });
+
   constructor(private _informacionService: InformacionService) {
 
-   }
+  }
 
   ngOnInit(): void {
     this._informacionService.getDatosFormulario().subscribe(data => {
       this._formulario = data.map(e => {
         return {
-          categoria: e.payload.doc.Categoria,
+          categoria: e.payload.doc.data().Categoria,
           colectivo: e.payload.doc.data().Colectivo,
           direccion: e.payload.doc.data().Direccion,
           dni: e.payload.doc.data().Dni,
@@ -30,10 +35,30 @@ export class InformacionComponent implements OnInit {
           p_covid4: e.payload.doc.data().p_covid4,
           p_covid5: e.payload.doc.data().p_covid5,
           p_covid6: e.payload.doc.data().p_covid6,
-          telefono: e.payload.doc.data().Telefono
+          telefono: e.payload.doc.data().Telefono,
+          paises: e.payload.doc.data().Paises_CCAA_visitados
         } as unknown as formulario;
+      });
+      console.log(this._formulario);
     });
-console.log(this._formulario);
-  });}
-
   }
+
+  getNombreFiltro(): any[] {
+
+    console.log('Form -->', this.informationForm.value.nombreFilter);
+
+    var filtrado: any[]
+    var nombre = "";
+
+    // filtrado = this._formulario.filter((item: any) =>
+    //   item.nombre.indexOf(nombre) !== -1);
+
+      filtrado = this._formulario.filter(
+        item => item.nombre === nombre);
+
+    console.log(" lista filtrada " + filtrado);
+
+    return filtrado
+  }
+
+}
